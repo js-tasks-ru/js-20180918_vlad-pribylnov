0dfb388bb192477566556498b089739d6608e9e8
Vlad Pribilnov <vlad.p@leadsmarket.com>
2018-10-13 18:35:14 +0300

    added modal window with questions

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,10 +1,16 @@
+'use strict';
+
 /**
  * Power
  * @param {number} m base
  * @param {number} n index
  * @returns {number}
  */
-function pow (m, n) {
+
+let number = prompt('Введите число', '');
+let powerOf = prompt('Введите степень', '');
+
+let pow = (m, n) => {
     let x = m;
 
     for (n;n > 1;n--) {
@@ -12,4 +18,11 @@ function pow (m, n) {
     }
 
     return m;
+};
+
+if (number >= 1 && powerOf >= 1) {
+    alert('Число ' + number + ' в степени ' + powerOf + ' = ' +  pow(+number, +powerOf));
+}  else {
+    alert('Степенью может быть только целое число не меньше 1')
 }
+
